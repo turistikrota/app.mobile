3929ed32f4d2e051686fcfe8104ba7a98cdb32ea
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-13 21:43:30 +0300

    update app icons

--- app/panel/_layout.tsx
+++ app/panel/_layout.tsx
@@ -1,36 +1,48 @@
+import { View } from "@gluestack-ui/themed";
 import { Stack } from "expo-router";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import AccountGuard from "~guards/account";
 import AuthGuard from "~guards/auth";
 import HeaderLeft from "~partials/layout/HeaderLeft";
 import HeaderTitle from "~partials/layout/HeaderTitle";
 
 export default function PanelLayout() {
+  const insets = useSafeAreaInsets();
   return (
     <AuthGuard.Optional>
       <AccountGuard.Optional>
-        <Stack
-          screenOptions={{
-            contentStyle: {
-              backgroundColor: "$white",
-            },
-            headerLeft: (props) => <HeaderLeft {...props} />,
-            headerTitle: (props) => <HeaderTitle {...props} />,
-            headerRight: () => null,
+        <View
+          sx={{
+            flex: 1,
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            bg: "$white",
           }}
-          initialRouteName="board"
         >
-          <Stack.Screen name="board" />
-          <Stack.Screen name="auth" />
-          <Stack.Screen
-            name="account"
-            options={{
-              headerShown: false,
+          <Stack
+            screenOptions={{
+              contentStyle: {
+                backgroundColor: "$white",
+              },
+              headerLeft: (props) => <HeaderLeft {...props} />,
+              headerTitle: (props) => <HeaderTitle {...props} />,
+              headerRight: () => null,
             }}
-          />
-          <Stack.Screen name="help/index" />
-          <Stack.Screen name="help/[id]" />
-        </Stack>
+            initialRouteName="board"
+          >
+            <Stack.Screen name="board" />
+            <Stack.Screen name="auth" />
+            <Stack.Screen
+              name="account"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen name="help/index" />
+            <Stack.Screen name="help/[id]" />
+          </Stack>
+        </View>
       </AccountGuard.Optional>
     </AuthGuard.Optional>
   );
