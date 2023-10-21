9bca24b9e4c3c76edfb92c14e6e76d163ddf9955
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-21 11:24:35 +0300

    fix the place detail

--- components/XMarkdown.tsx
+++ components/XMarkdown.tsx
@@ -1,25 +1,20 @@
-import { Box, Heading, Text } from "@gluestack-ui/themed";
+import { Heading, Image, Text } from "@gluestack-ui/themed";
 import React from "react";
-import { Dimensions, Linking } from "react-native";
-import FastImage from "react-native-fast-image";
+import { Dimensions } from "react-native";
 import Markdown from "react-native-markdown-renderer";
 
 type Props = {
   content: string;
 };
+
 const { width } = Dimensions.get("window");
 
-const openUrl = (url?: string) => {
-  if (!url) return;
-  Linking.openURL(url).catch(() => {});
-};
-
-const PlaceDetailMarkdownContentSection: React.FC<Props> = ({ content }) => {
+const XMarkdown: React.FC<Props> = ({ content }) => {
   return (
     <Markdown
       rules={{
         heading1: (node: any, children: any, parent: any, styles: any) => (
-          <Heading key={node.key} color="$textLight800" mb="$2">
+          <Heading key={node.key} color="$textLight800">
             {children}
           </Heading>
         ),
@@ -48,24 +43,22 @@ const PlaceDetailMarkdownContentSection: React.FC<Props> = ({ content }) => {
             {children}
           </Heading>
         ),
-        image: (node: any, children: any, parent: any, styles: any) => {
-          return (
-            <Box key={node.key} my="$1">
-              <FastImage
-                style={{
-                  width: width - 2 * 8,
-                  height: width * 0.7,
-                  borderRadius: 4,
-                }}
-                aria-label={node.attributes.alt}
-                source={{
-                  uri: node.attributes.src,
-                  priority: FastImage.priority.high,
-                }}
-              ></FastImage>
-            </Box>
-          );
-        },
+        image: (node: any, children: any, parent: any, styles: any) => (
+          <Image
+            key={node.key}
+            sx={{
+              w: width,
+              h: width * 0.7,
+              borderRadius: "$sm",
+              my: "$2",
+            }}
+            source={{
+              uri: node.attributes.src,
+            }}
+            alt={node.attributes.alt}
+            // add preview on click
+          ></Image>
+        ),
         strong: (node: any, children: any, parent: any, styles: any) => (
           <Text
             key={node.key}
@@ -82,7 +75,6 @@ const PlaceDetailMarkdownContentSection: React.FC<Props> = ({ content }) => {
             sx={{ fontWeight: "normal" }}
             color="$textLight600"
             size="md"
-            mb="$1"
           >
             {children}
           </Text>
@@ -93,7 +85,6 @@ const PlaceDetailMarkdownContentSection: React.FC<Props> = ({ content }) => {
             sx={{
               fontWeight: "normal",
               ml: "$2",
-              mb: "$1",
             }}
             color="$textLight600"
             size="md"
@@ -122,4 +113,4 @@ const PlaceDetailMarkdownContentSection: React.FC<Props> = ({ content }) => {
   );
 };
 
-export default PlaceDetailMarkdownContentSection;
+export default XMarkdown;
